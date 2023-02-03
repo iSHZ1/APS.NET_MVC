d2d3aaa382fe8e3e59e4726822ee5d0cfaf5cd96
IMacSHZ <Xardas366@gmail.com>
2023-02-03 18:48:17 +0400

    Done
    Done

---
+++ Lesson8/JobLessonASPNETMVCCore08v01/Models/Reports/ProductsCatalog.cs
@@ -0,0 +1,15 @@
+﻿using Orders.DAL.Entities;
+
+namespace JobLessonASPNETMVCCore08v01.Models.Reports
+{
+    public class ProductsCatalog
+    {
+        public string Name { get; set; } = null!;
+
+        public string Description { get; set; } = null!;
+
+        public DateTime CreationDate { get; set; }
+
+        public IEnumerable<Product> Products { get; set; }
+    }
+}
---
+++ Lesson8/JobLessonASPNETMVCCore08v01/Services/IProductReport.cs
@@ -0,0 +1,16 @@
+﻿namespace JobLessonASPNETMVCCore08v01.Services
+{
+    /// <summary>
+    /// Интерфейс, описывающий механизм генерации отчета
+    /// </summary>
+    public interface IProductReport
+    {
+        string CatalogName { get; set; }
+        string CatalogDescription { get; set; }
+        DateTime CreationDate { get; set; }
+
+        IEnumerable<(int id, string name, string category, decimal price)> Products { get; set; }
+
+        FileInfo Create(string reportTemplateFile);
+    }
+}
